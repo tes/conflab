63be7a06ef1a09d1e93f8a55dd331e62112f3d81
cliftonc <clifton.cunningham@gmail.com>
2018-06-15 15:55:04 +0100

    Remove etcd due to security issues and non-use

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var hostname = require('os').hostname().replace(/\..*/, '');
 var async = require('async');
-var Etcd = require('node-etcd');
-var EventEmitter = require('events').EventEmitter;
 var _ = require('lodash');
-var utils = require('./lib/utils');
 var minimist = require('minimist');
 var pathval = require('pathval');
 var stripBom = require('strip-bom');
@@ -15,7 +12,6 @@ var mergeDeep = _.partialRight(_.merge, function objectOnlyMerge(left, right) {
     if (_.isPlainObject(left) && _.isPlainObject(right)) {
         return _.merge(left, right, objectOnlyMerge);
     }
-
     return right;
 });
 
@@ -43,7 +39,6 @@ Config.prototype.load = function(options, next) {
 
     self.options = options;
     self.loaded = false;
-    self.heartbeatInterval = 10000;
 
     var configDir;
     var pm2Exec = process.env.pm_exec_path;
@@ -57,16 +52,13 @@ Config.prototype.load = function(options, next) {
     // This is the configuration that comes from the application it is included in
     self.libraryPath = options.libraryPath || process.env.CONFLAB_LIBRARY_CONFIG || path.join(__dirname, 'config');
     self.configPath = options.configPath || process.env.CONFLAB_CONFIG || path.join(configDir, 'config');
-    self.prefix = '/conflab';
 
     // Otherwise lets load up
     self.environment = options.env || process.env.CONFLAB_ENV || process.env.NODE_ENV || 'development';
-    self.events = new EventEmitter();
 
     // Local configs
     self.fileContent = {};
     self.fileConfig = {};
-    self.etcdConfig = {};
     self.config = {};
     self.ignoreExport = {};
 
@@ -75,14 +67,13 @@ Config.prototype.load = function(options, next) {
             return next(err);
         }
         self.loaded = true;
-        self.heartbeat();
         next(null, self.config);
     });
 
 }
 
 /**
- * Load configuration from files and then from etcd if appropriate
+ * Load configuration from options, files and argv
  */
 Config.prototype.loadConfig = function(next) {
 
@@ -91,38 +82,8 @@ Config.prototype.loadConfig = function(next) {
         self.loadFromOptions.bind(self),
         self.loadFromFiles.bind(self),
         self.loadFromArgv.bind(self),
-        self.loadFromEtcd.bind(self),
         self.mergeConfig.bind(self)
     ], next);
-}
-
-/**
- * Copy file content over to keys in etcd
- * This enables admin tools to display what is in the files
- * that are running.  It is brought up to date whenever the app
- * is started, no need to update in any other circumstance (so no ttl);
- */
-Config.prototype.putFilesInEtcd = function(next) {
-
-    var self = this;
-    if(!self.etcd) return next();
-
-    var loadFile = function(file, cb) {
-        if (self.ignoreExport[file]) return cb();
-        var fileJson = self.fileContent[file];
-        var etcdKey = path.join(self.etcdKeyBase, 'files', self.environment, file);
-        self.etcd.set(etcdKey, JSON.stringify(fileJson), cb);
-    }
-
-    var loadConfig = function(cb) {
-        var etcdKey = path.join(self.etcdKeyBase, 'files', self.environment, 'merged');
-        self.etcd.set(etcdKey, JSON.stringify(self.config), cb);
-    }
-
-    async.each(_.keys(self.fileContent),loadFile, function() {
-        loadConfig(next);
-    });
-
 }
 
 /**
@@ -134,70 +95,8 @@ Config.prototype.mergeConfig = function(next) {
     var self = this;
     self.config = {_:{}};
     self.config = mergeDeep(self.config, self.fileConfig);
-    self.config = mergeDeep(self.config, self.etcdConfig);
-    self.config._.on = self.events.on.bind(self.events);
-    self.config._.etcd = self.etcd;
-    self.config._.stop = self.watcher ? self.watcher.stop.bind(self.watcher) : function() {};
-    self.putFilesInEtcd(next);
+    next();
 
-}
-
-/**
- * Update a key on etcd every 10 seconds so that it nows the service is up.
- */
-Config.prototype.heartbeat = function() {
-    var self = this;
-    if(!self.etcd) return;
-    var hbKey = self.etcdKeyBase + '/heartbeat/' + self.environment;
-    self.etcd.set(hbKey, Date.now());
-    setTimeout(self.heartbeat.bind(self), self.heartbeatInterval);
-}
-
-/**
- * Load config from etcd - fails silently if no etcd config defined in files
- */
-Config.prototype.loadFromEtcd = function(next) {
-
-    var self = this;
-
-    var parseConfig = function(node, cb) {
-
-        self.etcdConfig = mergeDeep(self.etcdConfig, utils.objFromNode(node));
-
-        // Configure the watcher
-        self.watcher = self.etcd.watcher(self.etcdKey + '/', null, {recursive: true});
-        self.watcher.on('change', function(config) {
-            var key = config.node.key.replace(self.etcdKey + '/','');
-            pathval.set(self.etcdConfig, key, config.node.value);
-            self.mergeConfig();
-            self.events.emit('change');
-        });
-
-        cb();
-
-    }
-
-    if(!self.fileConfig.etcd) { return next(); }
-
-    // Etcd needs a service name to create the key
-    var packageJson = path.join(process.cwd(), 'package.json');
-    if(fs.existsSync(packageJson)) {
-       self.etcdKeyBase = self.prefix + '/' + require(packageJson).name;
-       self.etcdKey = self.etcdKeyBase + '/config/' + self.environment;
-    } else {
-        console.log('[CONFLAB] Error: You cant use etcd in a service without a name in the package.json');
-        return next();
-    }
-
-    self.etcd = new Etcd(self.fileConfig.etcd.hosts);
-
-    self.etcd.set(self.etcdKey + '/__', 'Ensure config can be watched', function(err) {
-        if (err) return next(err);
-        self.etcd.get(self.etcdKey, {recursive: true}, function(err, config) {
-            if (err) return next(err);
-            parseConfig(config.node, next);
-        });
-    });
 }
 
 Config.prototype.loadFromOptions = function(next) {
@@ -262,7 +161,6 @@ Config.prototype.loadFile = function(file, next) {
             // Doesn't over-ride
             self.fileContent[file.name] = _.cloneDeep(jsonData);
             self.fileConfig = mergeDeep(self.fileConfig, jsonData);
-            markForExport(file);
             return loadAdditionalFiles(file, next);
         });
     });
@@ -290,12 +188,6 @@ Config.prototype.loadFile = function(file, next) {
                 additional: true
             }, next);
         }
-    }
-
-    function markForExport(file) {
-        var exportToEtcd = self.fileContent[file.name].CF_exportToEtcd;
-        var explicit = exportToEtcd !== null && exportToEtcd !== undefined;
-        self.ignoreExport[file.name] = (explicit && !exportToEtcd) || (file.additional && !explicit);
     }
 
 }
